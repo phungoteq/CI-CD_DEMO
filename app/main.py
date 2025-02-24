96c06a1ce7754deb2837c42e3674cf3e10a2d8ca
phungoteq <phungo@teqnological.asia>
2025-02-24 10:08:13 +0700

    Demo 1

--- app/main.py
+++ app/main.py
@@ -2,9 +2,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
 
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
